e3fc61b284e52862022cab9c8dc57f7d9d8be9b6
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2019-03-13 04:30:18 +0600

    Remove USB Dependancy

--- install_chromium.sh
+++ install_chromium.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 # Copyright (c) 2019 Muntashir Al-Islam. All rights reserved.
 
-# USAGE: install_chromium.sh <usb> <efi_part> <root_a_part> <state_part>
-# Example: install_chromium.sh sdb sda4 sda5 sda6
+# USAGE: install_chromium.sh <chromiumos_image.img> <efi_part> <root_a_part> <state_part>
+# Example: install_chromium.sh ~/Downloads/ChromeOS/chromiumos_image.img sda4 sda5 sda6
 
 if [ $UID -ne 0 ]; then
     echo "This script must be run as root!"
     exit 1
 fi
-
-user=`logname`
-root="/home/${user}"
-efi_dir="${root}/efi"
-local_efi_dir="${root}/localefi"
-root_a="${root}/roota"
-local_root_a="${root}/localroota"
-state="${root}/state"
-local_state="${root}/localstate"
 
 #
 # Mount the partition if not already
@@ -34,25 +25,48 @@ function mountIfNotAlready {
 
 #
 # The main function
-# $1: USB device id e.g. sdb, sdc
+# $1: chromiumos_image.img
 # $2: HDD EFI-SYSTEM parition id e.g. sda4
 # $3: HDD ROOT-A partition id e.g. sda5
 # $4: HDD STATE partition id e.g. sda6
 function main {
     if [ $# -ne 5 ]; then
         echo "Invalid argument!"
-        echo "USAGE: install_chromium.sh <usb> <efi_part> <root_a_part> <state_part>"
+        echo "USAGE: install_chromium.sh <chromiumos_image.img> <efi_part> <root_a_part> <state_part>"
         exit 1
     fi
-    local usb="$1"
+    
+    local chrome_image=$1
+    local hdd_efi_part="/dev/$2"
+    local hdd_root_a_part="/dev/$3"
+    local hdd_state_part="/dev/$4"
+    
+    # Mount the image
+    local img_disk=`/sbin/losetup --show -fP "${chrome_image}"`
+    local img_efi_part="${img_disk}p12"
+    local img_root_a_part="${img_disk}p3"
+    local img_state_part="${img_disk}p1"
+    
+    local user=`logname`
+    if [ $? -ne 0 ]; then
+        user="chronos"
+    fi
+    local root="/home/${user}"
+    local efi_dir="${root}/efi"
+    local local_efi_dir="${root}/localefi"
+    local root_a="${root}/roota"
+    local local_root_a="${root}/localroota"
+    local state="${root}/state"
+    local local_state="${root}/localstate"
+    
     echo "ChromiumOS HDD installer"
     echo "------------------------"
     echo
     echo -n "Copying EFI-SYSTEM..."
     # Mount partition#12 (EFI-SYSTEM) of the image
-    mountIfNotAlready "${usb}12" "${efi_dir}"
+    mountIfNotAlready "${img_efi_part}" "${efi_dir}"
     # Mount the EFI-SYSTEM partition of the HDD
-    mountIfNotAlready "$2" "${local_efi_dir}"
+    mountIfNotAlready "${hdd_efi_part}" "${local_efi_dir}"
     # Delete all the contents of the local partition
     rm -Rf "${local_efi_dir}"/*
     # Copy files
@@ -66,9 +80,9 @@ function main {
 
     echo -n "Copying ROOT-A..."
     # Mount partition#3 (ROOT-A) of the image
-    mountIfNotAlready "${usb}3" "${root_a}"
+    mountIfNotAlready "${img_root_a_part}" "${root_a}"
     # Mount the ROOT-A partition of the HDD
-    mountIfNotAlready "$3" "${local_root_a}"
+    mountIfNotAlready "${hdd_root_a_part}" "${local_root_a}"
     # Delete all the contents of the local partition
     rm -Rf "${local_root_a}"/*
     # Copy files
@@ -82,9 +96,9 @@ function main {
 
     echo -n "Copying STATE..."
     # Mount partition#1 (STATE) of the image
-    mountIfNotAlready "${usb}1" "${state}"
+    mountIfNotAlready "${img_state_part}" "${state}"
     # Mount the STATE partition of the HDD
-    mountIfNotAlready "$4" "${local_state}"
+    mountIfNotAlready "${hdd_state_part}" "${local_state}"
     # Delete all the contents of the local partition
     rm -Rf "${local_state}"/*
     # Copy files
